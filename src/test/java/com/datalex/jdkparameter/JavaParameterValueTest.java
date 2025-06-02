fb2662f99683fa9e5e0766e2357ce00b165d9310
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-06-02 17:02:20 +0200

    Migrate tests to JUnit5
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods
    * Minor code cleanup

--- src/test/java/com/datalex/jdkparameter/JavaParameterValueTest.java
+++ src/test/java/com/datalex/jdkparameter/JavaParameterValueTest.java
@@ -2,58 +2,59 @@ package com.datalex.jdkparameter;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.JDK;
 import java.util.List;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 
-public class JavaParameterValueTest {
+@WithJenkins
+class JavaParameterValueTest {
 
-    @Rule
-    public JenkinsRule j = new JenkinsRule();
+    private static final JDK JDK_11 = new JDK("jdk-11", "/my/jdk/11");
+    private static final JDK JDK_17 = new JDK("jdk-17", "/my/jdk/17");
+    private static final JDK JDK_21 = new JDK("jdk-21", "/my/jdk/21");
+    private static final JDK CURRENT = new JDK("current-jdk", System.getProperty("java.home"));
 
     private FreeStyleProject project;
     private FreeStyleBuild build;
     private JDK projectJDK;
 
-    private static final JDK jdk11 = new JDK("jdk-11", "/my/jdk/11");
-    private static final JDK jdk17 = new JDK("jdk-17", "/my/jdk/17");
-    private static final JDK jdk21 = new JDK("jdk-21", "/my/jdk/21");
-    private static final JDK current = new JDK("current-jdk", System.getProperty("java.home"));
+    private JenkinsRule j;
 
-    @Before
-    public void configureJDKsAndrunOneJob() throws Exception {
-        j.jenkins.setJDKs(List.of(jdk11, jdk17, jdk21, current));
+    @BeforeEach
+    void setUp(JenkinsRule rule) throws Exception {
+        j = rule;
+        j.jenkins.setJDKs(List.of(JDK_11, JDK_17, JDK_21, CURRENT));
         project = j.createFreeStyleProject();
-        projectJDK = jdk21;
+        projectJDK = JDK_21;
         project.setJDK(projectJDK);
         build = j.buildAndAssertSuccess(project);
     }
 
     @Test
-    public void testGetSelectedJDK() {
+    void testGetSelectedJDK() {
         String selectedJDK = projectJDK.getName();
         JavaParameterValue value = new JavaParameterValue("JDK_NAME", "JDK description", selectedJDK);
         assertThat(value.getSelectedJDK(), is(selectedJDK));
     }
 
     @Test
-    public void testSetSelectedJDK() {
-        String selectedJDK = jdk17.getName();
+    void testSetSelectedJDK() {
+        String selectedJDK = JDK_17.getName();
         JavaParameterValue value = new JavaParameterValue("JDK_NAME", "JDK 17 description", selectedJDK);
         value.setSelectedJDK(selectedJDK);
         assertThat(value.getSelectedJDK(), is(selectedJDK));
     }
 
     @Test
-    public void testCreateBuildWrapper() {
-        String selectedJDK = jdk11.getName();
+    void testCreateBuildWrapper() {
+        String selectedJDK = JDK_11.getName();
         JavaParameterValue value = new JavaParameterValue("JDK_NAME", "JDK 11 description", selectedJDK);
         JavaParameterBuildWrapper wrapper = (JavaParameterBuildWrapper) value.createBuildWrapper(build);
         assertTrue(wrapper.isJdkIsAvailable());
@@ -61,7 +62,7 @@ public class JavaParameterValueTest {
     }
 
     @Test
-    public void testCreateBuildWrapperWithDefaultJDK() {
+    void testCreateBuildWrapperWithDefaultJDK() {
         String selectedJDK = JavaParameterDefinition.DEFAULT_JDK;
         JavaParameterValue value = new JavaParameterValue("JDK_NAME", "Default JDK description", selectedJDK);
         JavaParameterBuildWrapper wrapper = (JavaParameterBuildWrapper) value.createBuildWrapper(build);
